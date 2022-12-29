841e081e99749e65f5cd78df6c43e86f481ae959
unknown <joacohernandez157@gmail.com>
2022-12-29 10:33:29 -0300

    first commit

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
-const ImgBaseUrl = "https://image.tmdb.org/t/p/original";
-/* REFERENCIA A LOS BOTONES  */ 
-const btnContainer = document.querySelector(`.pagination`)
-const previusPageBtn = document.querySelector(`.left`)
-const nextPageBtn = document.querySelector(`.right`)
-const cardsContainer = document.querySelector(`.cards__container`)
-const pageNumber = document.querySelector(`.pages`)
-const filterButtons = document.querySelectorAll(`.btn`)
-const filterContainer = document.querySelector (`.options__movie`)
+const buscador = document.querySelector(`#buscador`)
+const btnEnviar = document.querySelector(`.btn__search`)
+const form = document.querySelector(`#form`)
+const containerClima = document.querySelector(`.container__clima`)
 
-/* Necesitamos manejar el total de paginas   */ 
-
-const pageController = {
-    page: null,
-    total: null,
-    searchParameter: TRENDING_URL
-}
- const renderLoader = () => { //Este nos devuelve un string con html
-    return `
-        <div class="lds-rings">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>    
-    `//retorna el loader
- }
- //renderizar las cartas por cada una de pelis
-const renderCards = () => {
-    cardsContainer.innerHTML = movies.map(movie => getMovieHTML(movie)).join("")
-}
-const getMovieHTML = ({title,release_date,vote_average,poster_path}) => {
-    return `
-    <div class="card">
-        <img
-            class ="imagen" 
-            alt = "${}"   
-            >
-    `
-}
-const getMovies = async () => {
-    //Mostrar loader
-    cardsContainer.innerHTML= renderLoader()
-    //Llamar api
-    const movies = await fetchMovies(pageController.searchParameter)
-    //actualizar page controller
-    pageController.total = movies.total_pages
-    pageController.page = movies.page
-   //manejar la pagination
-    //renderizar cards
-    renderCards (movies.results)
+const pasarKelvinAc = (grados) => {
+    const gradosFinal = Math.floor(grados - 273)
+    return gradosFinal
 }
 
+const getCityHTML = (city) => {  
+return `
+<div class ="climatotal">
+<div class="clima__izquierda"> 
+            <h2 class="localidad">${city.name}</h2>
+            
+            <p class="estado">${city.weather[0].description}</p>
+            <span class="temperatura__actual">${pasarKelvinAc(city.main.temp)}°</span>
+        </div class="clima__medio">
+            <img class ="soleado"src="./assets/soleado.jpg" alt="foto soleado">
+        <div class="clima__derecha">
+            <span class="max"><i class="fa-solid fa-arrow-up"></i>temp max:${pasarKelvinAc(city.main.temp_max)}°</span>
+            <span class="min"><i class="fa-solid fa-arrow-down"></i>temp min:${pasarKelvinAc(city.main.temp_min)}°</span>
+            <p class="humedad">${city.main.humidity}% humedad</p>
+        </div>
+        </div>`
+}
 
-/* ACA SE VA A CARGAR TODO  */
+let cities = JSON.parse(localStorage.getItem(`cities`)) || [];
+let renderCitiesList = (cities) => {
+    containerClima.innerHTML= cities.map(city => getCityHTML(city)).join("")
+}
+const saveLocalStorage = cities => {
+    localStorage.setItem(`Cities`, JSON.stringify(cities))
+}
+const searchCity = async (e) => {
+    e.preventDefault();
+    const searchedCity = buscador.value.trim()
+    if (searchedCity === ``) {
+        alert(`Esta vacio`)
+        return
+    }
+    const fetchedCity = await requestCity(searchedCity)
+    if (!fetchedCity.id) {
+        alert(`Ciudad ingresa no existe`)
+        form.reset()
+        return
+    }
+    else {
+
+    }
+    cities = [fetchedCity,...cities]
+    renderCitiesList(cities);
+    saveLocalStorage(cities);
+    form.reset()
+}
+
 const init = () => {
-    window.addEventListener(`DOMContentLoaded`, getMovies)
+    renderCitiesList(cities)
+    form.addEventListener(`submit`, searchCity)
 }
-
 init()
--- request.js
+++ request.js
@@ -1,12 +1,12 @@
-const KEY = "362c5b12f1ca01b9a8cf97ed8697a368";
 
-const TRENDING_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}&language=en-US`;
-const TOPRATED_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&language=en-US`;
-const UPCOMING_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${KEY}&language=en-US`;
+const API_key = `5c7cbbd351ee0d1209f5e54c1dbfc8ad`
 
-const fetchMovies = async (searchUrl, page = 1) => {
-    const response = await fetch (`${searchUrl}&page=${page}`)
+const requestCity = async (city) => {
+    const mainURL= `https://api.openweathermap.org/data/2.5/weather`
+    const queryParams = `?q=${city}&appid=${API_key}`
+    const response = await fetch(mainURL + queryParams)
     const responseData = await response.json()
     console.log(responseData)
-    return fetchMovies
+    return responseData;
 }
+requestCity(`Rosario`)
